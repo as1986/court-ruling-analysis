fc4ed8f7ab6bf2d8beb77f0ea0f0f5d462e76076
Chu-Cheng Lin <chuchenglin@cmu.edu>
2015-09-27 09:35:40 -0400

    gets print page

--- crawler.py
+++ crawler.py
@@ -20,6 +20,7 @@ class Crawler:
                         'kw':'', 'keyword':'', 'sdate':'', 'edate':'', 'jud_title':'', 'jmain':'',
                         'Button':'查詢', 'searchkw': '賴素如'
                         }
+        self.prefix = 'http://210.69.124.221/FJUD/'
 
 
     def query(self, term, court = u'SLD 臺灣士林地方法院'):
@@ -33,15 +34,18 @@ class Crawler:
 
     def get(self, page):
         results = self.__session.get(page, headers={'referer':'http://jirs.judicial.gov.tw/FJUD/FJUDQRY02_1.aspx'})
-        results.encoding = 'utf-8'
-        return results.text
+        ruling = BeautifulSoup(results.text)
+        friendly = ruling.findAll('a', text='友善列印')[0]
+        friendly_results = self.__session.get(self.prefix+friendly['href'],
+                                              headers={'referer':'http://jirs.judicial.gov.tw/FJUD/FJUDQRY03_1.aspx'})
+        friendly_results.encoding = 'utf-8'
+        return friendly_results.text
 
     def cleanse(self, crawled):
         soup = BeautifulSoup(crawled)
-        return soup.get_text()
+        return soup('pre')[0].get_text()
 
     def get_links(self, query_results):
-        prefix = 'http://210.69.124.221/FJUD/'
         to_return = []
         soup = BeautifulSoup(query_results)
         entries = soup.findAll("table", { "id" : "Table3" })
@@ -52,7 +56,7 @@ class Crawler:
             for l in links:
                 cand = l['href']
                 if cand is not None and cand is not '#':
-                    to_return.append(prefix+cand)
+                    to_return.append(self.prefix+cand)
 
         return to_return
 
